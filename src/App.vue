09b1fe9fc256a0fde64fb5e36e7045ee58ff16f5
13inuo0 <13inuo0@github.com>
2025-10-21 12:12:39 +0900

    로그인 페이지 로그인 로그아웃 상태 변경 및 저장

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <Local_ex01/>
+  <Local_ex01 />
+
+  <hr />
+  <div class="container">
+    <Review_01 />
+    <hr />
+    <Login />
+  </div>
 </template>
 
 <script setup>
-import Local_ex01 from './components/Local_ex01.vue';
-
+import Local_ex01 from "./components/Local_ex01.vue";
+import Login from "./components/Login.vue";
+import Review_01 from "./components/Review_01.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
